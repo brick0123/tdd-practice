0cd8d3f1a70ed672616a58d3089ea85708e536db
brick0123 <tlfanfvk@naver.com>
2021-03-21 23:09:03 +0900

    테스트 우선 개발

--- text-refinder/index.test.js
+++ text-refinder/index.test.js
@@ -1,11 +1,49 @@
-const sut = require("./index")
+const faker = require("faker");
+const sut = require("./index");
+
+test.each`
+  source                 | expected
+  ${"hello  world"}      | ${"hello world"}
+  ${"hello   world"}     | ${"hello world"}
+  ${"hello    world"}    | ${"hello world"}
+  ${"hello     world"}   | ${"hello world"}
+  ${"hello      world"}  | ${"hello world"}
+  ${"hello       world"} | ${"hello world"}
+`('sut transforms "$source" to "$expected"', 
+({ source, expected }) => {
+  const actual = sut(source);
+  expect(actual).toBe(expected);
+});
+
 
 test.each`
   source              | expected
-  ${"hello  world"}   | ${"hello world"}
-  ${"hello   world"}  | ${"hello world"}
-  ${"hello    world"} | ${"hello world"}
-`('sut transforms "$source" to "$expected"', ({ source, expected }) => {
+  ${"hello\t world"}  | ${"hello world"}
+  ${"hello \tworld"}  | ${"hello world"}
+`('sut transforms "$source" that contains tab charactor to "$expected"', 
+({ source, expected }) => {
   const actual = sut(source);
   expect(actual).toBe(expected);
+});
+
+test.each`
+source             | bannedWords              | expected
+${"hello mockist"} | ${["mockist", "purist"]} | ${"hello *******"}
+${"hello purist"}  | ${["mockist", "purist"]}  | ${"hello ******"}
+`('sut transforms "$source" to $"expected"', 
+({ source, bannedWords, expected}) => {
+  const actual = sut(source, { bannedWords });
+  expect(actual).toBe(expected);
+});
+
+describe("given banned word", () => {
+  const bannedWord = faker.lorem.word();
+  const masked = "*".repeat(bannedWord.length);
+  const source = `hello ${bannedWord}`;
+  const options = { bannedWords: [bannedWord] };
+
+  test(`"${bannedWord}" when invoke sut then it returns "hello ${masked}"`, () => {
+    const actual = sut(source, options);
+    expect(actual).toBe(`hello ${masked}`);
+  });
 });
