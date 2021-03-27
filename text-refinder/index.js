7a01a69e489c014dd080eeb73144f72f860d9ca2
brick0123 <tlfanfvk@naver.com>
2021-03-27 22:51:38 +0900

    리팩터링

--- text-refinder/index.js
+++ text-refinder/index.js
@@ -1,22 +1,29 @@
-function refineText(s, options) {
-  s = s
-    .replace("    ", " ")
-    .replace("\t ", " ")
-    .replace(" \t", " ")
-    .replace("  ", " ")
-    .replace("  ", " ")
-    .replace("  ", " ")
-    .replace("mockist", "*******")
-    .replace("purist", "******");
+function refineText(source, options) {
+  return [normalizeWhiteSpaces, compactWithSpaces, maskBannedWords].reduce(
+    (value, filter) => filter(value, options),
+    source
+  );
+}
 
-  if (options) {
-    for (const bannedWord of options.bannedWords) {
-      const mask = "*".repeat(bannedWord.length);
-      s = s.replace(bannedWord, mask);
-    }
-  }
+function maskBannedWords(source, options) {
+  return options 
+  ? options.bannedWords.reduce(maskBannedWord, source) 
+  : source;
+}
 
-  return s;
+function maskBannedWord(source, bannedWord) {
+  const mask = "*".repeat(bannedWord.length);
+  return source.replace(bannedWord, mask);
+}
+
+function normalizeWhiteSpaces(source) {
+  return source.replace("\t", " ");
+}
+
+function compactWithSpaces(source) {
+  return source.indexOf("  ") < 0 
+  ? source 
+  : compactWithSpaces(source.replace("  ", " "));
 }
 
 module.exports = refineText;
